79411741177aa4443ee0845f982c36969b6ecd44
Robert Chen <robchen401@gmail.com>
2014-09-14 23:47:04 -0400

    initial commit: ex1-3

---
+++ ex01_02/simple_first_program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    for (int i = 0; i<10; i++)
+    {
+        printf("hello\n");
+        printf("World\n");
+        puts("nice\n");
+    }
+    return 0;
+}
---
+++ ex03/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+    char country[12] = "America";
+
+    printf("I am %d years old and was born in %s.\n", age, country);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
